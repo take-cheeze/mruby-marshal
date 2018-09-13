c11f555a58e7e60b84af8c2cf611bb1b9358616d
take-cheeze <takechi101010@gmail.com>
2018-09-13 17:57:47 +0900

    Include header.

--- src/marshal.cpp
+++ src/marshal.cpp
@@ -2,6 +2,7 @@
 #include <mruby/array.h>
 #include <mruby/class.h>
 #include <mruby/hash.h>
+#include <mruby/khash.h>
 #include <mruby/string.h>
 #include <mruby/value.h>
 #include <mruby/variable.h>
