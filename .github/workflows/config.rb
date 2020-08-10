cdb1939ffe5d5208108b4030934864e15d2caa16
take-cheeze <takechi101010@gmail.com>
2020-08-10 16:58:29 +0900

    Add sanitizer flags

--- .github/workflows/config.rb
+++ .github/workflows/config.rb
@@ -4,6 +4,10 @@ MRuby::Build.new do |conf|
   enable_bintest
   enable_debug
 
+  conf.cc.flags << '-fsanitize=address,undefined'
+  conf.cxx.flags << '-fsanitize=address,undefined'
+  conf.linker.flags << '-fsanitize=address,undefined'
+
   conf.gem "#{MRUBY_ROOT}/.."
   conf.cxx.flags << '-std=c++11'
 end
