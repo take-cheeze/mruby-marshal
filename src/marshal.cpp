e11008162559aee69fc11f4f91637a67f15aa560
take-cheeze <takechi101010@gmail.com>
2020-08-16 12:05:06 +0900

    Use khash in 1.x

--- src/marshal.cpp
+++ src/marshal.cpp
@@ -308,7 +308,7 @@ write_context<Out>& write_context<Out>::marshal(mrb_value const& v, mrb_int limi
         fixnum(mrb_hash_size(M, v));
         auto meta = hash_marshal_meta{*this, limit};
         mrb_hash_foreach(M, RHASH(v), &marshal_hash_each, &meta);
-#else
+#elif MRUBY_RELEASE_MAJOR >= 2 && MRUBY_RELEASE_MINOR >= 0
         mrb_value const keys = mrb_hash_keys(M, v);
         mrb_funcall(M, keys, "sort!", 0);
 
@@ -316,6 +316,14 @@ write_context<Out>& write_context<Out>::marshal(mrb_value const& v, mrb_int limi
         for(mrb_int i = 0; i < RARRAY_LEN(keys); ++i) {
           mrb_value const k = RARRAY_PTR(keys)[i];
           marshal(k, limit).marshal(mrb_hash_get(M, v, k), limit);
+        }
+#else
+        khash_t(ht) const * const h = RHASH_TBL(v);
+
+        fixnum(kh_size(h));
+        for(khiter_t k = kh_begin(h); k != kh_end(h); ++k) {
+          if (!kh_exist(h, k)) { continue; }
+          marshal(kh_key(h, k), limit).marshal(kh_value(h, k).v, limit);
         }
 #endif
 
